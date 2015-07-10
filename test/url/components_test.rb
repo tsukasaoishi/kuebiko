50bd465fe81db4f9f3428acdd630d82830a81497
tsukasaoishi <tsukasa.oishi@gmail.com>
2015-07-11 00:50:51 +0900

    Added test

--- test/url/components_test.rb
+++ test/url/components_test.rb
@@ -27,4 +27,22 @@ class ComponentsTest < Minitest::Test
     @klass.class_eval { trailing_slash true }
     assert @klass.components.trailing_slash
   end
+
+  test "can specify more than one value" do
+    _schema = :bbb
+    _host = "oishi.com"
+    _port = 2015
+
+    @klass.class_eval do
+      schema _schema
+      host _host
+      port _port
+      trailing_slash true
+    end
+
+    assert_equal _schema, @klass.components.schema
+    assert_equal _host, @klass.components.host
+    assert_equal _port, @klass.components.port
+    assert @klass.components.trailing_slash
+  end
 end
