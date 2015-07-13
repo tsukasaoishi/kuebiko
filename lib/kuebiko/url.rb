76d50662bade40187647e1691c6ad3108c0c9622
tsukasaoishi <tsukasa.oishi@gmail.com>
2015-07-13 22:00:58 +0900

    Implemented internal accessor

--- lib/kuebiko/url.rb
+++ lib/kuebiko/url.rb
@@ -5,15 +5,27 @@ module Kuebiko
     extend Components::ForUrl
 
     class << self
-      private
+      def resource(*names)
+        class_eval <<-DEF_INIT, __FILE__, __LINE__ + 1
+          def initialize(*resources, **options)
+            #{names.map{|n| "@_#{n}"}.join(", ")}, _dust = resources
+            @_options = options
+          end
 
-      def schema_value
-        if components.schema?
-          components.schema
-        else
-          Kuebiko::Components.schema
-        end
+          #{names.map{|n| "def #{n}; @_#{n}; end"}.join("\n")}
+          private #{names.map{|n| ":#{n}"}.join(", ")}
+        DEF_INIT
       end
+    end
+
+    def initialize(*resources, **options)
+      @_options = options
+    end
+
+    private
+
+    def options
+      @_options
     end
   end
 end
---
+++ test/url/resource_test.rb
@@ -0,0 +1,53 @@
+require 'test_helper'
+
+
+class ResourceTest < Minitest::Test
+  Article = Struct.new(:title)
+  User = Struct.new(:name)
+
+  def setup
+    @klass = Class.new(Kuebiko::Url)
+  end
+
+  test "use as internal accessor" do
+    title = "first_day"
+    @klass.class_eval { resource :article }
+    inst = @klass.new(Article.new(title))
+
+    assert_equal title, inst.instance_eval { article.title }
+  end
+
+  test "can specify more than one resource" do
+    title = "second_day"
+    name = "tsuka"
+    @klass.class_eval { resource :article, :user }
+    inst = @klass.new(Article.new(title), User.new(name))
+
+    assert_equal title, inst.instance_eval { article.title }
+    assert_equal name, inst.instance_eval { user.name }
+  end
+
+  test "internal accessor returns nil if you do not pass to initializer" do
+    title = "third_day"
+    @klass.class_eval { resource :article, :user }
+    inst = @klass.new(Article.new(title))
+
+    assert_equal title, inst.instance_eval { article.title }
+    assert_nil inst.instance_eval { user }
+
+    inst2 = @klass.new
+    assert_nil inst2.instance_eval { article }
+    assert_nil inst2.instance_eval { user }
+  end
+
+  test "can specify options" do
+    time_num = Time.now.to_i
+    inst = @klass.new(tsu: time_num)
+    assert_equal time_num, inst.instance_eval { options[:tsu] }
+
+    @klass.class_eval { resource :article, :user }
+    some_code = rand(100000)
+    inst2 = @klass.new(oi: some_code)
+    assert_equal some_code, inst2.instance_eval { options[:oi] }
+  end
+end
